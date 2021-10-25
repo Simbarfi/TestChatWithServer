4461b9e354f89e549944d692a71beaeee075b056
Sharad Ranpara <ranpas56@uwosh.edu>
2021-10-24 19:47:50 -0500

    Add project files.

---
+++ MVVM/Model/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatClient.MVVM.Model
+{
+    class UserModel
+    {
+        public string Username { get; set; }
+        public string  Image { get; set; }
+
+        public string UID { get; set; }
+    }
+}
